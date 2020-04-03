e4815c29656d871bacc7d0f584f8c6cbd846cf05
John Taggart <taggartj1985@gmail.com>
2020-04-03 14:13:08 +0100

    added some tests for game class all passing

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,43 @@ Game.delete_all
 
 publisher1 = Publisher.new({"name" => "Fantasy Flight Games"})
 publisher1.save
-publisher2 = Publisher.new({"name" => "Days Of Wonder"})
-publisher2.save
 publisher3 = Publisher.new({"name" => "Babox Games"})
 publisher3.save
-publisher4 = Publisher.new({"name" => "Asmodee"})
-publisher4.save
+publisher2 = Publisher.new({"name" => "Space Cowboys"})
+publisher2.save
 
 game1 = Game.new({"name" => "Game Of Thrones",
-                  "description" => "2-player Card Game",
+                  "description" => "2-player Card Gamen based on the hit tv show!",
                   "stock" => 10,
                   "rrp" => 13,
                   "price" => 8,
                   "publisher_id" => publisher1.id,
                   "theme" => "Living Card Game"})
-game1.save
+                  game1.save
+game2 = Game.new({"name" => "Kune v Lakia",
+                  "description" => "2-player Card Game, About a divourced bunnies fighting over possessions",
+                  "stock" => 5,
+                  "rrp" => 10,
+                  "price" => 5,
+                  "publisher_id" => publisher3.id,
+                  "theme" => "Deck Builder"})
+                  game2.save
+game3 = Game.new({"name" => "Splendor",
+                  "description" => "2-4 player, Collect chips and buy resources",
+                  "stock" => 0,
+                  "rrp" => 22,
+                  "price" => 14,
+                  "publisher_id" => publisher3.id,
+                  "theme" => "Board Game"})
+                  game3.save
+game4 = Game.new({"name" => "Lord Of The Rings",
+                  "description" => "2-4player, Adventure through Mordor in this campaign game",
+                  "stock" => 6,
+                  "rrp" => 60,
+                  "price" => 45,
+                  "publisher_id" => publisher1.id,
+                  "theme" => "Board Game"})
+                  game4.save
 
 
 
