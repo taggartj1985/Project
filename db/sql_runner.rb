9201b325519f97b6b205bca4c241b32112a2960e
John Taggart <taggartj1985@gmail.com>
2020-04-03 11:24:18 +0100

    game initilize set up

---
+++ db/sql_runner.rb
@@ -0,0 +1,11 @@
+require("minitest/autorun")
+require("minitest/reporters")
+require_relative("../model/game")
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class GameTest< MiniTest::Test
+
+
+
+
+end
--- model/game.rb
+++ model/game.rb
@@ -1,5 +1,15 @@
 class Game
 
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @description = options['description']
+    @quantity = options['quantity'].to_i
+    @rrp = options['rrp'].to_i
+    @price = options['price'].to_i
+    @publisher_id = options['publisher_id'].to_i
+    @theme = options['theme']
+  end
 
 
 
